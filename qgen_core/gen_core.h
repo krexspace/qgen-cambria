dde3863c994c2594be959f161c7eb5164999efb8
Infinity <kreatorspace@gmail.com>
2019-03-30 13:01:42 -0700

    First working version, generates cube scene

---
+++ qgen_core/gen_core.h
@@ -0,0 +1,87 @@
+
+// use the fbxsdk.h
+#include <fbxsdk.h>
+
+namespace qg {
+	// to create an instance of the SDK manager
+	bool InitializeSdkObjects(
+		FbxManager*& pSdkManager,
+		FbxScene*& pScene
+	);
+
+
+	// to destroy an instance of the SDK manager
+	void DestroySdkObjects(
+		FbxManager* pSdkManager,
+		bool pExitStatus
+	);
+
+	// to save a scene to a FBX file
+	bool Export(
+		const char* pFilename,
+		int pFileFormat
+	);
+
+	// to save a scene to a FBX file
+	bool SaveScene(
+		FbxManager* pSdkManager,
+		FbxDocument* pScene,
+		const char* pFilename,
+		int pFileFormat,
+		bool pEmbedMedia
+	);
+
+	// to create a basic scene
+	bool CreateScene();
+
+	// Create a marker to use a point of interest for the camera. 
+	FbxNode* CreateMarker(
+		FbxScene* pScene,
+		char* pName
+	);
+
+	// Create a camera
+	FbxNode* CreateCamera(
+		FbxScene* pScene,
+		char* pName
+	);
+
+	void SetCameraPointOfInterest(
+		FbxNode* pCamera,
+		FbxNode* pPointOfInterest
+	);
+
+	void SetMarkerDefaultPosition(
+		FbxNode* pMarker
+	);
+
+	void SetCameraDefaultPosition(
+		FbxNode* pCamera
+	);
+
+	// to get the root node
+	const FbxNode* GetRootNode();
+
+	// to get the root node name
+	const char * GetRootNodeName();
+
+	// create a new cube under the root node
+	void CreateCube(bool pWithTexture, bool pAnimate);
+
+	void CreateCubeDetailed(
+		char* pCubeName,
+		double pX,
+		double pY,
+		double pZ,
+		int pRotateAxe,
+		bool pWithTexture,
+		bool pAnim
+	);
+
+	// Create a cube mesh
+	FbxNode* CreateCubeMesh(
+		FbxScene* pScene,
+		char* pName
+	);
+
+}
